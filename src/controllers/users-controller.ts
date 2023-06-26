13622986574603bb268a586c17076832c33e066b
Nanda-Souza <fesouoli@gmail.com>
2023-06-25 23:54:42 -0300

    feat: Update User Profile method v2

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -23,12 +23,17 @@ export async function usersPost(req: Request, res: Response, next: NextFunction)
 
 export async function usersUpdate(req: AuthenticatedRequest, res: Response, next: NextFunction) {
   try {    
-    await userService.updateUser({
+    const user = await userService.updateUser({
       id: req.userId,
       ...req.body,      
-    });
+    });   
 
-    return res.sendStatus(httpStatus.OK);
+    return res.status(httpStatus.OK).json({      
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      img_url: user.img_url
+    });
   } catch (e) {    
     next(e);
   }
